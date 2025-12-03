55a6589c379f7af13ba5d23f1398487d3ef9df4a
Ethan Brews <ebrews@amazon.com>
2025-12-03 12:05:38 +0000

    template

---
+++ file_helper.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"bufio"
+	"os"
+)
+
+func ReadLines(path string) ([]string, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close() // Ignore Error
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+
+	return lines, nil
+}
---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"os"
+	"strconv"
+	"time"
+)
+
+var funcDayMap = []func() int{
+	day01,
+}
+
+func main() {
+	args := os.Args[1:]
+	var day int
+	if len(args) == 0 {
+		date := time.Now()
+		if date.Year() != 2025 || date.Day() > 12 || date.Month() != time.December {
+			panic("Advent of code has ended!")
+		}
+		day = time.Now().Day()
+	} else {
+		var err error
+		day, err = strconv.Atoi(args[0])
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	print(funcDayMap[day-1]())
+}
